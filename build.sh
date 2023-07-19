2548222d28d6c4c62572c315cd7ded9627bce30b
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2023-07-19 18:02:05 +0900

    add sudo command

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@ GIT_USERNAME=`git config user.name`
 GIT_USEREMAIL=`git config user.email`
 GIT_SSHKEY=~/.ssh/id_rsa
 
+SUDO_STRING=`groups|grep docker`
+SUDO=""
+if [ -z "$SUDO_STRING" ]; then
+  SUDO="sudo "
+fi
+
 while [[ $# -gt 0 ]]; do
     case $1 in
         -u|--user)
@@ -34,7 +40,7 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-DOCKER_BUILDKIT=1 docker build \
+DOCKER_BUILDKIT=1 $SUDO docker build \
     --build-arg GIT_USERNAME="$GIT_USERNAME" \
     --build-arg GIT_USEREMAIL="$GIT_USEREMAIL" \
     --ssh default=$GIT_SSHKEY \
