a92e1c1d2f32c5d6a3361921aec748033682c6ee
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-07-24 10:02:32 +0900

    add local.cmake

---
+++ local.cmake
@@ -0,0 +1,4 @@
+set(EXTENSIONS_DIR ${CMAKE_CURRENT_LIST_DIR}/superbuild-extensions)
+include(${EXTENSIONS_DIR}/controllers/BaseLineWalkingController.cmake)
+include(${EXTENSIONS_DIR}/gui/mc_rtc-magnum.cmake)
+include(${EXTENSIONS_DIR}/interfaces/mc_mujoco.cmake)
